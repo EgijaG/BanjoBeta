80f3eaf698075d2db0df9f7a737c9a0a091fd7b8
EgijaG <egija.graudina@gmail.com>
2021-11-08 01:06:43 +0200

    Added logging functionality

--- CharacterGetter.py
+++ CharacterGetter.py
@@ -1,14 +1,26 @@
-
-from asyncio.log import logger
+import logging
+import logging.config
+import yaml
 from configparser import ConfigParser
 from marvel import Marvel
 
+# Loading the loggin config
+with open('log_characterGetter.yaml', 'r') as stream:
+    config = yaml.safe_load(stream)
+logging.config.dictConfig(config)
 
-config = ConfigParser()
-config.read('config.ini')
+# Creating the logger
+logger = logging.getLogger('root')
+logger.info("Marvel comics data retrieval service")
+
+
 try:
+    config = ConfigParser()
+    config.read('config.ini')
+
     marvel_pub_key = config.get('marvel_config', 'PUB_KEY')
     marvel_priv_key = config.get('marvel_config', 'PRIV_KEY')
+
     m = Marvel(marvel_pub_key, marvel_priv_key)
     characters = m.characters
 except:
