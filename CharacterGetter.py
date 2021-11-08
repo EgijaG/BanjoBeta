26e8b04c43c2e72baf6771f7a901e52befb4d6e6
EgijaG <egija.graudina@gmail.com>
2021-11-08 13:44:09 +0200

    Added more complex data extraction

--- CharacterGetter.py
+++ CharacterGetter.py
@@ -13,22 +13,55 @@ logging.config.dictConfig(config)
 logger = logging.getLogger('root')
 logger.info("Marvel comics data retrieval service")
 
+charName = ""
+comicList = dict()
 
-try:
-    config = ConfigParser()
-    config.read('config.ini')
 
-    marvel_pub_key = config.get('marvel_config', 'PUB_KEY')
-    marvel_priv_key = config.get('marvel_config', 'PRIV_KEY')
+def checkConfigandAPI():
+    try:
+        config = ConfigParser()
+        config.read('config.ini')
 
-    m = Marvel(marvel_pub_key, marvel_priv_key)
-    characters = m.characters
-except:
-    logger.exception('')
-logger.info('DONE')
+        marvel_pub_key = config.get('marvel_config', 'PUB_KEY')
+        marvel_priv_key = config.get('marvel_config', 'PRIV_KEY')
 
-x = 1011335
-for i in range(0, 10):
-    all_cahracters = characters.comics(x)
-    for a in range(1, 12):
-        print(all_cahracters['data']['results'][int(a)]['title'])
+        m = Marvel(marvel_pub_key, marvel_priv_key)
+        characters = m.characters
+    except:
+        logger.exception('')
+    logger.info(
+        'Succesfully read configuration and established connection with API')
+
+    charBegin = "Captain America"
+    all_characters = characters
+
+    # Serial code for the character
+    wintSId = getCharacters(charBegin, all_characters)
+    getWhereCharacterParticipated(characters, wintSId)
+    print(comicList)
+
+
+# Getting the characters form Marvel Cinematic Universe(MCU) by the name provided when the fucntion is called
+def getCharacters(nameStartsW, all_char):
+    global charName
+    all_char = all_char.all(nameStartsWith=nameStartsW)
+    for i in range(len(all_char['data']['results'])):
+        print(all_char["data"]["results"][i]["id"],
+              all_char["data"]["results"][i]["name"])
+        charName = all_char["data"]["results"][i]["name"]
+        return all_char["data"]["results"][i]["id"]
+
+# Getting the comics where this particular character has appeared in
+
+
+def getWhereCharacterParticipated(characters, charId):
+    all_char = characters.comics(charId)
+    print("The " + charName + " is seen on: ")
+    for i in range(len(all_char['data']['results'])):
+        print(all_char['data']['results'][int(i)]['title'])
+        item = {(all_char['data']['results'][int(i)]['title']): int(i)}
+        comicList.update(item)
+
+
+# calling the first method
+checkConfigandAPI()
